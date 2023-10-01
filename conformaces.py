815f1f5032f6c2233f02bd5504a312ea95620c18
isaacbest <zicolas4ever@yahoo.com>
2023-10-02 00:10:44 +0200

    recent

--- conformaces.py
+++ conformaces.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import pm4py
 
+df = pm4py.format_dataframe(pd.read_csv("flight_event_log.csv", sep=","), case_id="Flight", activity_key="Activity",
+                            timestamp_key="Timestamp")
+
+
+
+
+
+pn, im, fm = pm4py.discover_petri_net_inductive(df)
+pm4py.view_petri_net(pn, im, fm)
+
+pm4py.fitness_token_based_replay(df, pn, im, fm)
